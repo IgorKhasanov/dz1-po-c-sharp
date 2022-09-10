a8ccb3e0198ace38654cd9d2e308fe82e6be4baf
Igor Khasanov <igor-igor-73@mail.ru>
2022-09-10 16:54:16 +0500

    Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.WriteLine("Введите положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= num)
+{
+    int x = count % 2;
+    if (x == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count++;
+}
